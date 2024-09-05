34532ed07e083127fe40ce439071b01d53fa756e
Sgal Cheung <zhangsigao@live.com>
2024-09-05 20:44:56 +0800

    Finished Astro and Xata starter guides.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly XATA_API_KEY: string;
+  readonly XATA_BRANCH?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
